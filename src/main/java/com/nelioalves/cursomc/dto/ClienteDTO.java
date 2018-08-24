9f3e6329806610780945120bc7ccea978501571b
Alexandre Barbosa de Freitas <freitas.alexandre@totvs.com.br>
2018-08-24 13:19:56 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -6,7 +6,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO {
 
 	private static final long serialVersionUID = 1L;
